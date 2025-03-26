6276300c4562add64356e323e84e894885a58e11
Illia Bilous <bilous.job@yahoo.com>
2025-03-26 16:11:24 +0200

    feat: font, metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Mona_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const monaSans = Mona_Sans({
+  variable: "--font-mona-sans",
   subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "Imaginify",
-  description: "AI-powered Image Generator",
+  title: "Saas ai voice manager",
+  description: "Saas ai voice manager",
 };
 
 export default function RootLayout({
@@ -23,9 +18,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${monaSans.variable} ${monaSans.variable} antialiased`}
       >
         {children}
       </body>
