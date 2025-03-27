407aa22a439d13accf964f119922e6e6dc686e5c
Illia Bilous <bilous.job@yahoo.com>
2025-03-27 11:50:15 +0200

    feat: root layout logo

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,23 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import { ReactNode } from 'react'
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
+  // TODO: logo as a component
   return (
-    <div>{ children }</div>
+    <div className="root-layout">
+      <Link href="/" className="flex flex-row items-center gap-2">
+        <Image 
+          src="/logo.svg"
+          width={38}
+          height={32}
+          alt="logo"
+        />
+        <h2 className="text-primary-100">PrepWise</h2>
+      </Link>
+      
+      {children}
+    </div>
   )
 }
 
